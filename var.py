ac26820c38e08bf553b3b4957664375eb0c6c330
molee1354 <molee1354@gmail.com>
2022-07-26 15:34:18 -0400

    this

--- var.py
+++ var.py
@@ -10,7 +10,7 @@ output_root = "E:/mokin/behavior_classification/outputs"
 
 # task/trial id
 TASK_ID = "Moon_1x"
-TRIAL_ID = "extract_test2"
+TRIAL_ID = "extract_3"
 
 angles = np.linspace(20, 70, 11, dtype=int)
 
