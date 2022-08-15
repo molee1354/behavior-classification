c4a46fd6b9d878f57116287082583f0385ec92b3
molee1354 <molee1354@gmail.com>
2022-08-16 02:17:59 +0900

    path settings

--- var.py
+++ var.py
@@ -2,17 +2,17 @@
 import numpy as np
 
 # where the necessary data is stored
-extract_root = "/home/mlee80/projects/behavior-classification/data_Extracts"
+extract_root = "/run/media/moosung/9C33-6BBD/Shared/Research/Granular/Code/Output_Refactor/data_Extracts"
 
-raw_root = "/scratch/mlee80/lmp_dumps/earth_raw"
+raw_root = "/run/media/moosung/9C33-6BBD/Shared/Research/Granular/data/lmp_dumps/lmpDump_LIS02"
 
 # where the outputs should go
-output_root = "/home/mlee80/projects/behavior-classification/outputs"
+output_root = "/home/moosung/projects/Research/Granular/behavior-classification/outputs"
 
 
 # task/trial id
-TASK_ID = "Moon_1x"
-TRIAL_ID = "extract_3"
+TASK_ID = "LIS02"
+TRIAL_ID = "testing7"
 
 angles = np.linspace(20, 70, 11, dtype=int)
 
