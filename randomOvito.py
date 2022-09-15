70f2a892c83aa984c468b10b4f7b83f459813709
molee1354 <molee1354@gmail.com>
2022-09-15 16:17:38 -0400

    human classification helper updated

--- randomOvito.py
+++ randomOvito.py
@@ -1,3 +1,4 @@
+import os
 import json
 from time import sleep
 import numpy as np
@@ -9,13 +10,16 @@ sleep(5)
 #* C:\Users\moosu\projects\Research\lammps_LIS01\Output\lmpDump_LIS01\lmpDump_A20\iteration_V1.0_A20\dmp.reg.LIS01_V1.0_A20
 class Filepaths:
 
-    def __init__(self,task:str, angle: int, vel: float) -> None:
-        self.path_name = f"C:\\Users\\moosu\\projects\\Research\\lammps_LIS01\\Output\\lmpDump_{task}\\lmpDump_A{angle}\\iteration_V{vel}_A{angle}\\dmp.reg.{task}_V{vel}_A{angle}"
+    # def __init__(self,task:str, angle: int, vel: float) -> None:
+    #     # self.path_name = f"C:\\Users\\moosu\\projects\\Research\\lammps_LIS01\\Output\\lmpDump_{task}\\lmpDump_A{angle}\\iteration_V{vel}_A{angle}\\dmp.reg.{task}_V{vel}_A{angle}"
+    #     self.path_name = f"/run/media/moosung/9C33-6BBD/Shared/Research/Granular/data/task_Bennu_1x"
 
-        # print(self.path_name)
+    #     # print(self.path_name)
+    def __init__( self,sim_root:str ) -> None:
+        self.sim_root = sim_root
     
     @staticmethod
-    def do_process( pathName):
+    def do_process( pathName ):
         sleep(1.5)
         # pg.hotkey('alt','tab') #switching back to ovito
         pg.hotkey('ctrl','i')
@@ -23,25 +27,30 @@ class Filepaths:
         pg.press('enter')
         pg.press('enter')
 
-try:
-    task = sys.argv[1]
-except IndexError:
-    task = "LIS01"
-    
-velocities = np.linspace(1.0, 7.0, 13)
-angles = np.linspace(20, 70, 11, dtype=int)
+sim_filepath = f"/run/media/moosung/9C33-6BBD/Shared/Research/Granular/data/task_Bennu_1x"
 
-pathNames = []
-for angle in angles:
-    for velocity in velocities:
-        path = Filepaths(task, angle, velocity)
+# try:
+#     task = sys.argv[1]
+# except IndexError:
+#     task = "LIS01"
 
-        pathNames.append(path.path_name)
+reg_files = [reg for reg in os.listdir(sim_filepath) if "dmp.reg" in reg]
 
-shuffle(pathNames)
+path_names = []
+for file in reg_files:
+    path = Filepaths( file )
+    path_names.append(path.sim_root)
+
+# for angle in angles:
+#     for velocity in velocities:
+#         path = Filepaths(task, angle, velocity)
+
+#         pathNames.append(path.path_name)
+
+shuffle(path_names)
 
 inputBehavior = {}
-for idx, element in enumerate(pathNames):
+for idx, element in enumerate(path_names):
     Filepaths.do_process(element)
     inputBehavior[element[-8:]] = input(f"[{idx}] Input behavior: ")
 
