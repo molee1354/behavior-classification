91913cdf377264d8e6ffae0b8353448d83a9189e
molee1354 <molee1354@gmail.com>
2022-11-19 16:24:38 -0500

    add catch for all ValueError cases

--- extractTest.py
+++ extractTest.py
@@ -57,83 +57,77 @@ def main():
     for angle in var.angles:
         for path in [ f"{extractsPath}/{p}" for p in os.listdir(extractsPath) if f"_A{angle}" in p ]:
 
-            #* creating an output object that holds the necessary data
-            output = Comparer( path )
-
-            #! no human decision
-            # human_dec = output.behavior_obj.human_decision
-
-            # information for conditionals and such all included for better analysis
-            # comp_dec = {
-            #     "behavior" : output.behavior_obj.computer_decision,
-            #     "confidence": output.behavior_obj.confidence,
-            #     "contact_pIDs": output.behavior_obj.contact_pIDs,
-            #     "airborne": output.behavior_obj.airborne,
-            #     "surpasses": output.behavior_obj.surpasses,
-            #     "crater_out": output.behavior_obj.crater_out
-            # }
-            comp_dec = output.behavior_obj
-            
-            #* plotting the output behavior and saving the figure
-            plotter = Visualizer(comp_dec)
-
-            save_path = f"{var.output_root}/output_plots/path_plots/path_plots_{TASK_ID}_{TRIAL_ID}"
-            os.makedirs(save_path, exist_ok=True)
-            
-            # plotting function
             try:
+                #* creating an output object that holds the necessary data
+                output = Comparer( path )
+
+                comp_dec = output.behavior_obj
+                
+                #* plotting the output behavior and saving the figure
+                plotter = Visualizer(comp_dec)
+
+                save_path = f"{var.output_root}/output_plots/path_plots/path_plots_{TASK_ID}_{TRIAL_ID}"
+                os.makedirs(save_path, exist_ok=True)
+                
+                # plotting function
                 plotter.path_plots(save_path, "svg")
+
+                #* saving the computer behavior
+                #todo Export <<output.behavior_obj.decision>> this here
+                #! comp_dec is a tuple --> (behavior, confidence)
+                compDict[ re.findall( "V[0-9]+\.?[0-9]+_A[0-9]*",path )[0] ] = {
+                    "behavior" : comp_dec.behavior,
+                    "confidence": comp_dec.confidence,
+                    "contact_pIDs": comp_dec.contact_pIDs,
+                    # "airborne": comp_dec.airborne,
+                    "surpasses": comp_dec.surpasses,
+                    "crater_out": comp_dec.crater_out,
+
+                    #TODO normalizing this can also be an option
+                    "crater_final_x": comp_dec.crater_final, # final disc x position with respect to the final crater position
+                    "crater_min_y": comp_dec.crater_min_y,
+                    "mound_final_x": comp_dec.mound_final, # final disc x position with respect to the final mound position
+                    "disc_final_x": comp_dec.disc_final_x,
+                    "disc_final_y": comp_dec.disc_final_y, # final disc height
+                    "disc_max_y": comp_dec.disc_max_y
+                }
+
+                
+                #* printing the behavior output
+                # if human_dec == comp_dec["behavior"]:
+                #     behavior_str = f"Computer / Human: [{ comp_dec['behavior'] }/{human_dec}]\tConfidence: [{comp_dec['confidence']}]"
+                # else:
+                #     # behavior_str = f"Computer / Human: [{comp_dec[0]}/{human_dec}]\tConfidence: [{comp_dec[1]}] <-- UNMATCHING" 
+                #     behavior_str = f"Computer / Human: [{ comp_dec['behavior'] }/{human_dec}]\tConfidence: [{comp_dec['confidence']}] <-- UNMATCHING" 
+                #     unmatching.append(f"V{velocity}_A{angle}") 
+
+                behavior_str = f"Behavior : [{ comp_dec.behavior }]\tConfidence: [{comp_dec.confidence}]"
+
+                logs_savepath = f"{var.output_root}/output_logs"
+                logs_savefile = f"{logs_savepath}/extract_{TASK_ID}_{TRIAL_ID}.log"
+                if os.path.exists(logs_savefile):
+                    os.remove(logs_savefile)
+
+                os.makedirs(logs_savepath, exist_ok=True)
+                logging.basicConfig( 
+                    level = logging.INFO, 
+                    format = "  %(message)s", 
+
+                    # outputting to both a log file and the stdout 
+                    handlers = [logging.FileHandler(logs_savefile), logging.StreamHandler(sys.stdout)]
+                ) 
+                logging.info(f"\n Iteration: {comp_dec.iteration}\t{behavior_str}") 
+                # printing the reasons line by line 
+                for idx, reason in enumerate(output.behavior_obj.reasons): 
+                    logging.info(f"\tReason {idx}: {reason}") 
+
+                # outputting the final vote 
+                logging.info(f"\tVotes: {output.behavior_obj.decisionDict}")
+
             except ValueError:
                 print(f"ValueError occured in {path}")
                 pass
 
-            #* saving the computer behavior
-            #todo Export <<output.behavior_obj.decision>> this here
-            #! comp_dec is a tuple --> (behavior, confidence)
-            compDict[ re.findall( "V[0-9]+\.?[0-9]+_A[0-9]*",path )[0] ] = {
-                "behavior" : comp_dec.behavior,
-                "confidence": comp_dec.confidence,
-                "contact_pIDs": comp_dec.contact_pIDs,
-                # "airborne": comp_dec.airborne,
-                "surpasses": comp_dec.surpasses,
-                "crater_out": comp_dec.crater_out,
-
-                #TODO normalizing this can also be an option
-                "crater_final_x": comp_dec.crater_final, # final disc x position with respect to the final crater position
-                "crater_min_y": comp_dec.crater_min_y,
-                "mound_final_x": comp_dec.mound_final, # final disc x position with respect to the final mound position
-                "disc_final_x": comp_dec.disc_final_x,
-                "disc_final_y": comp_dec.disc_final_y, # final disc height
-                "disc_max_y": comp_dec.disc_max_y
-            }
-
-            
-            #* printing the behavior output
-            # if human_dec == comp_dec["behavior"]:
-            #     behavior_str = f"Computer / Human: [{ comp_dec['behavior'] }/{human_dec}]\tConfidence: [{comp_dec['confidence']}]"
-            # else:
-            #     # behavior_str = f"Computer / Human: [{comp_dec[0]}/{human_dec}]\tConfidence: [{comp_dec[1]}] <-- UNMATCHING" 
-            #     behavior_str = f"Computer / Human: [{ comp_dec['behavior'] }/{human_dec}]\tConfidence: [{comp_dec['confidence']}] <-- UNMATCHING" 
-            #     unmatching.append(f"V{velocity}_A{angle}") 
-
-            behavior_str = f"Behavior : [{ comp_dec.behavior }]\tConfidence: [{comp_dec.confidence}]"
-
-            logs_savepath = f"{var.output_root}/output_logs"
-            os.makedirs(logs_savepath, exist_ok=True)
-            logging.basicConfig( 
-                level = logging.INFO, 
-                format = "  %(message)s", 
-
-                # outputting to both a log file and the stdout 
-                handlers = [logging.FileHandler(f"{logs_savepath}/extract_{TASK_ID}_{TRIAL_ID}.log"), logging.StreamHandler(sys.stdout)]
-            ) 
-            logging.info(f"\n Iteration: {comp_dec.iteration}\t{behavior_str}") 
-            # printing the reasons line by line 
-            for idx, reason in enumerate(output.behavior_obj.reasons): 
-                logging.info(f"\tReason {idx}: {reason}") 
-
-            # outputting the final vote 
-            logging.info(f"\tVotes: {output.behavior_obj.decisionDict}")
                 
 
     # saving the computer behavior classifications    
